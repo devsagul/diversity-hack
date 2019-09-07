0dabacd3b86ed8bd19dd0be8adf96ff6b4c641f9
Valeriy Klimenko / ShnurD6 / bbaelor- <shnurd6@mail.ru>
2019-09-07 18:00:34 +0300

    Add O3 comilation flag

--- tree_custom_order/setup.py
+++ tree_custom_order/setup.py
@@ -6,10 +6,10 @@
 #    By: ShnurD6 <shnurd6@mail.ru>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/09/07 17:08:13 by ShnurD6           #+#    #+#              #
-#    Updated: 2019/09/07 17:09:12 by ShnurD6          ###   ########.fr        #
+#    Updated: 2019/09/07 17:59:31 by ShnurD6          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from distutils.core import setup, Extension
 setup(name = 'TcoModule', version = '1.0',  \
-   ext_modules = [Extension('TcoModule', ['tree_custom_order.cpp'])])
+   ext_modules = [Extension('TcoModule', ['tree_custom_order.cpp'], extra_compile_args = ["-O3"])])
